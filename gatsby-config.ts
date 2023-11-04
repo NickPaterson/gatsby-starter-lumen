d0cb06a05960d65e9ba638edad0983def15abec2
Nick <Npgb18@gmail.com>
2023-11-04 22:41:13 +0000

    favicon is a png

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -157,7 +157,7 @@ export default {
         short_name: config.title,
         theme_color: "hsl(31, 92%, 62%)",
         background_color: "hsl(0, 0%, 100%)",
-        icon: "content/favicon.jpg",
+        icon: "content/favicon.png",
         display: "standalone",
         start_url: "/",
       },
